d71b5d67e69e8458ef34285af91b5ce050cb2460
Duong Thuan <duongthuan204@gmail.com>
2026-09-05 12:47:03 +0700

    Enhancement

--- src/skill/taisinh.js
+++ src/skill/taisinh.js
@@ -35,7 +35,7 @@ class TaiSinh extends React.Component {
                     </div>
                     <br className="is-mobi" />
                     {ball === 0 ? <div className="button is-danger is-light is-rounded my-2">Hãy học kĩ năng Nghề trước</div> : 
-                        <div className="button is-info is-light is-rounded my-2">Được chọn&nbsp;<b>{ball-learn}</b>&nbsp;kĩ năng Hệ</div>}
+                        <div className="button is-info is-light is-rounded my-2">Còn&nbsp;<b>{ball-learn}</b>&nbsp;slot kĩ năng trống</div>}
                     <div>
                         {he !== 'hoa' ? <Dia skills={skills} update={update} tooltip={this.tooltip} /> : ''}
                         {he !== 'phong' ? <Thuy skills={skills} update={update} tooltip={this.tooltip} /> : ''}
